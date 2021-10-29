d1faff083ff09509ef73cb39628fe3dea9be3dc0
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-10-29 12:40:19 +0300

    check if a play with that name is created and if it is throw  error else create it

--- services/playService.js
+++ services/playService.js
@@ -10,6 +10,12 @@ async function getPlayById(id) {
 
 
 async function createPlay(playData) {
+    const pattern = new Regexp(`^${playData.title}$`, 'i')
+    const existing = await Play.find({ title: { $regex: pattern } });
+    if (existing) {
+        throw new Error('A play with this name already exists');
+    };
+
     const play = new Play(playData);
     await play.save();
     return play;
