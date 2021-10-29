54b754b123c21171ec7c00f934b7fe20d54dfe6b
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-10-29 10:59:03 +0300

    created our Play model

--- models/Play.js
+++ models/Play.js
@@ -1,6 +1,12 @@
 const { Schema, model } = require('mongoose');
 
-const schema = new Schema({});
-
+const schema = new Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true, maxlength: 50 },
+    imageUrl: { type: String, required: true },
+    public: { type: Boolean, default: false },
+    createdAt: { type: Date, required: true },
+    usersLiked: [{ type: Schema.Types.ObjectId, ref: "User", default: [] }],
+});
 
 module.exports = model('Play', schema);
