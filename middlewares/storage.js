ea5ea6d1c2ad9ab0a855d1d55597df6462e6a47b
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-10-29 11:05:21 +0300

    create playService and import it in the req.storage which is gl can be used globally

--- middlewares/storage.js
+++ middlewares/storage.js
@@ -1,5 +1,9 @@
+const playService = require('../services/playService');
+
 module.exports = (req, res, next) => {
     //TODO import and decorate services
-    req.storage = {};
+    req.storage = {
+        ...playService
+    };
 
 };
