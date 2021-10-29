454616dd63f06a4c77a74409d648414bf1365468
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-10-29 12:19:17 +0300

    if we are verified we will have the userData everywhere now

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -74,6 +74,7 @@ function parseToken(req, res) {
         try {
             const userData = jwt.verify(token, TOKEN_SECRET);
             req.user = userData;
+            res.locals.user = userData;
         } catch (error) {
             res.clearCookie(COOKIE_NAME);
             res.redirect('/auth/login');
